d006ffd63963cdd7bcf9b84a01bf5a174b14b08c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-04-14 16:08:31 +0530

    LO: writing responses

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import http from "http";
-console.log("testNode");
+import http from "node:http";
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  const writeWithDelay = async () => {
+    for (let counter = 1; counter < 10; counter++) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      res.write("heartbeat\n");
+    }
+    // End the response after the loop completes
+    res.end("a basic server");
+  };
+  writeWithDelay();
+});
+
+server.listen(PORT, () => console.log("Server running on port:" + PORT));
