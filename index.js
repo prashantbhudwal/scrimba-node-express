7685f4eddc7cc825a8c34c616a4a11ed8bb1f605
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-04-15 13:40:35 +0530

    Refactor server to handle requests asynchronously and improve readability

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import http from "node:http";
+import { getData } from "./data.js";
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  const writeWithDelay = async () => {
-    for (let counter = 1; counter < 10; counter++) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      res.write("heartbeat\n");
-    }
-    // End the response after the loop completes
-    res.end("a basic server");
-  };
-  writeWithDelay();
+const server = http.createServer(async (req, res) => {
+  console.log(req.url); // which url the request hits
+  if (req.url === "/") {
+    const writeWithDelay = async () => {
+      for (let counter = 1; counter < 10; counter++) {
+        // await new Promise((resolve) => setTimeout(resolve, 1000));
+        res.write("heartbeat\n");
+      }
+      // End the response after the loop completes
+      res.end("a basic server");
+    };
+    writeWithDelay();
+  }
+
+  if (req.url === "/api" && req.method === "GET") {
+    const destinations = await getData();
+    res.end(JSON.stringify(destinations));
+  }
 });
 
 server.listen(PORT, () => console.log("Server running on port:" + PORT));
